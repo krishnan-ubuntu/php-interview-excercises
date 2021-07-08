a7845284632ec9a222eb638dc174123e5d363c92
Krishnan <ks@geedesk.com>
2021-07-08 20:10:08 +0530

    check palindrome

---
+++ check_for_palindrome_string.php
@@ -0,0 +1,48 @@
+<?php 
+/*
+This question is about cchecking if the provided string is a palindrome without using strrev
+ */
+function isPalindrome($string = false) 
+{
+	if (empty($string)) 
+	{
+		echo 'Please provide a string';
+	}
+	else  
+	{
+		$invertedStr = '';
+		$santizedStr = sanitizeInput($string);
+		$stingArr 	 = str_split($santizedStr);
+		$stingArrLen = count($stingArr);
+
+		for ($i=($stingArrLen - 1); $i >= 0; $i--) 
+		{
+			$invertedStr = $invertedStr.$stingArr[$i];
+		}
+
+		if ($string === $invertedStr) 
+		{
+			echo 'The provided string is planidrome';
+			/*or
+			return true;*/
+		}
+		else 
+		{
+			echo 'The provided string is not a planidrome';
+			/*or
+			return false;*/
+		}
+	}
+}
+
+
+function sanitizeInput($string) 
+{
+	$string = trim($string);
+  	$string = stripslashes($string);
+  	$string = htmlspecialchars($string);
+  	return $string;
+}
+
+
+isPalindrome('racecar');
